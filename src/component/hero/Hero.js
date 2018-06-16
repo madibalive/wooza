28799cba38806a18ba50e6cbb57d89e5e3f640d0
madiba <madibafeed@outlook.com>
2018-06-16 05:55:03 +0000

    fixed info

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -26,49 +26,66 @@ import parse from "parse";
  * />
  */
 const Hero = props => (
-  <Row noGutters="true">
-    <Col xs="12" sm="12" md="8" style={{ height: "460px" }}>
-      <img
-        style={{ objectFit: "cover" }}
-        src={props.video.get("poster100")}
-        className="img  w-100 h-100"
-        alt=""
-      />
-    </Col>
-    <Col
-      xs="12"
-      sm="12"
-      md="4"
-      className="p-4 banner-large-subtle"
-      style={{ height: "460px" }}
-    >
-      <h2 className=" font-weight-bold text-white">
-        {props.video ? props.video.get("title") : <Skeleton />}
-      </h2>
+  <div
+    style={{ height: "760px" }}
+    className="Hero"
+    style={{
+      backgroundImage: `url(${props.video.get("poster100")})`
+    }}
+  >
+    <div className=" herooverlay">
+      <Container >
+        <h2 className=" ml-4 font-weight-bold text-white">
+          {props.video ? props.video.get("title") : <Skeleton />}
+        </h2>
+      </Container>
+      <Container>
+        <Row noGutters="true" className="m-4">
+          <Col
+            xs="12"
+            sm="12"
+            md="12"
+            className="p-4"
+            style={{ height: "460px" }}
+          >
+            <img
+              style={{ objectFit: "cover" }}
+              src={props.video.get("poster100")}
+              className="img  w-100 h-100 shadow"
+              alt=""
+            />
+          </Col>
+          <Col xs="12" sm="6" md="6" lg="4" className="p-4">
+            {/* <h2 className=" font-weight-bold text-white">
+              {props.video ? props.video.get("title") : <Skeleton />}
+            </h2> */}
 
-      <small className="text-white">
-        {props.video.get("production")}
-      </small>
-      <p className="text-white">
-        {props.video.get("year")} | runtime {props.video.get("runtime")}
-      </p>
-      <p> {props.video && props.video.get("desc").substring(0, 350)}</p>
-      <p className="text-white">Genre | {props.video.get("genre")}</p>
-      <button
-        onClick={() => {
-          this.props.history.push({
-            pathname: `/movies/${props.video.id}`,
-            state: { video: props.video }
-          });
-        }}
-        className="btn btn-primary btn-large text-white"
-      >
-        watch now
-      </button>
-    </Col>
-  </Row>
-
-  // <div>
+            <small className="text-white">
+              {props.video.get("production")}}
+            </small>
+            <p className="text-white">
+              {props.video.get("year")} | runtime {props.video.get("runtime")}
+            </p>
+            <p className="text-white">Genre | {props.video.get("genre")}</p>
+            <button
+              onClick={() => {
+                this.props.history.push({
+                  pathname: `/movies/${props.video.id}`,
+                  state: { video: props.video }
+                });
+              }}
+              className="btn btn-primary btn-large text-white"
+            >
+              watch now
+            </button>
+          </Col>
+          <Col xs="12" sm="6" md="6" lg="8" className="p-4 text-white">
+            <p> {props.video && props.video.get("desc")}</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  </div>
   //   {/* //   className="Hero"
   // //   // style={{
   // //   //   backgroundImage: `url(${props.video.get("poster100")})`
